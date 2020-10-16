7aa97ae74d1713376b599a20775c9edb3b0ad688
Crowdbotics <team@crowdbotics.com>
2020-10-16 19:09:01 +0000

    Update components

--- src/features/BlankScreen0145309/screens/blank.js
+++ src/features/BlankScreen0145309/screens/blank.js
@@ -37,22 +37,28 @@ export default class Blank extends React.Component {
       />
       <TextInput
         placeholder="Welcome to the MindNRG MINDWAVE Player"
-        editable={true}
+        editable={false}
         multiline={true}
         style={styles.TextInput_5}
         value={this.state.TextInput_5}
         onChangeText={nextValue => this.setState({ TextInput_5: nextValue })}
       />
       <Button
-        title="Update"
+        title="Login"
         color="#9132ff"
         style={styles.Button_7}
+        onPress={() => this.props.navigation.navigate("BlankScreen1145546")}
+      />
+      <Button
+        title=""
+        color="#ffffff"
+        style={styles.Button_9}
         onPress={() => alert("Pressed!")}
       />
       <Button
-        title="Login"
+        title="Update App"
         color="#9132ff"
-        style={styles.Button_9}
+        style={styles.Button_11}
         onPress={() => alert("Pressed!")}
       />
     </View>
@@ -249,6 +255,160 @@ const styles = StyleSheet.create({
     lineHeight: 12,
     letterSpacing: 0
   },
+
+  View_1: {
+    marginLeft: 0,
+    marginRight: 0,
+    marginTop: 0,
+    marginBottom: 0,
+    paddingLeft: 0,
+    paddingRight: 0,
+    paddingTop: 0,
+    paddingBottom: 0,
+    overflow: "visible",
+    alignSelf: "center",
+    fontSize: 18,
+    color: "#8e74ec",
+    backgroundColor: "#ffffff",
+    fontStyle: "normal",
+    fontWeight: "normal",
+    flexDirection: "column",
+    flexWrap: "wrap",
+    justifyContent: "flex-start",
+    alignItems: "stretch",
+    alignContent: "stretch",
+    borderColor: "#000000",
+    borderStyle: "solid",
+    borderWidth: 0,
+    borderLeftWidth: 0,
+    borderRightWidth: 0,
+    borderTopWidth: 0,
+    borderBottomWidth: 0,
+    borderRadius: 0
+  },
+
+  Image_3: {
+    width: 150,
+    height: 100,
+    marginLeft: 0,
+    marginRight: 0,
+    marginTop: 15,
+    marginBottom: 0,
+    paddingLeft: 0,
+    paddingRight: 0,
+    paddingBottom: 0,
+    overflow: "hidden",
+    alignSelf: "center",
+    borderColor: "#000000",
+    borderStyle: "solid",
+    borderWidth: 0,
+    borderLeftWidth: 0,
+    borderRightWidth: 0,
+    borderTopWidth: 0,
+    borderBottomWidth: 0,
+    borderRadius: 0
+  },
+
+  TextInput_5: {
+    width: "75%",
+    height: 75,
+    marginLeft: 0,
+    marginRight: 0,
+    marginTop: 5,
+    marginBottom: 5,
+    paddingLeft: 5,
+    paddingRight: 5,
+    paddingTop: 5,
+    paddingBottom: 5,
+    overflow: "visible",
+    alignSelf: "center",
+    fontSize: 18,
+    color: "#9132ff",
+    backgroundColor: "#ffffff",
+    fontStyle: "normal",
+    fontWeight: "bold",
+    borderColor: "#000000",
+    borderStyle: "solid",
+    borderWidth: 0,
+    borderLeftWidth: 0,
+    borderRightWidth: 0,
+    borderTopWidth: 0,
+    borderBottomWidth: 0,
+    borderRadius: 0,
+    textAlign: "center",
+    textAlignVertical: "center",
+    textDecorationLine: "none",
+    textTransform: "none",
+    lineHeight: 18,
+    letterSpacing: 0
+  },
+
+  Button_7: {
+    width: "50%",
+    height: 25,
+    marginLeft: 0,
+    marginRight: 0,
+    marginTop: 5,
+    marginBottom: 0,
+    paddingLeft: 5,
+    paddingRight: 5,
+    paddingTop: 5,
+    paddingBottom: 5,
+    overflow: "visible",
+    alignSelf: "center",
+    fontSize: 12,
+    color: "#ffffff",
+    backgroundColor: "#ffffff",
+    fontStyle: "normal",
+    fontWeight: "bold",
+    borderColor: "#000000",
+    borderStyle: "solid",
+    borderWidth: 0,
+    borderLeftWidth: 0,
+    borderRightWidth: 0,
+    borderTopWidth: 0,
+    borderBottomWidth: 0,
+    borderRadius: 0,
+    textAlign: "center",
+    textAlignVertical: "center",
+    textDecorationLine: "none",
+    textTransform: "none",
+    lineHeight: 15,
+    letterSpacing: 0
+  },
+
+  Button_9: {
+    width: "50%",
+    marginLeft: 0,
+    marginRight: 0,
+    marginTop: 5,
+    marginBottom: 5,
+    paddingLeft: 5,
+    paddingRight: 5,
+    paddingTop: 5,
+    paddingBottom: 5,
+    overflow: "visible",
+    alignSelf: "center",
+    fontSize: 12,
+    color: "#ffffff",
+    backgroundColor: "#ffffff",
+    fontStyle: "normal",
+    fontWeight: "bold",
+    borderColor: "#000000",
+    borderStyle: "solid",
+    borderWidth: 0,
+    borderLeftWidth: 0,
+    borderRightWidth: 0,
+    borderTopWidth: 0,
+    borderBottomWidth: 0,
+    borderRadius: 0,
+    textAlign: "center",
+    textAlignVertical: "center",
+    textDecorationLine: "none",
+    textTransform: "none",
+    lineHeight: 12,
+    letterSpacing: 0
+  },
   View_1: {
     marginLeft: 0,
     marginRight: 0,
@@ -302,7 +462,7 @@ const styles = StyleSheet.create({
   },
   TextInput_5: {
     width: "75%",
-    height: 75,
+    height: 100,
     marginLeft: 0,
     marginRight: 0,
     marginTop: 5,
@@ -334,19 +494,19 @@ const styles = StyleSheet.create({
     letterSpacing: 0
   },
   Button_7: {
-    width: "50%",
-    height: 25,
+    width: "100%",
+    height: 20,
     marginLeft: 0,
     marginRight: 0,
     marginTop: 5,
-    marginBottom: 0,
-    paddingLeft: 5,
+    marginBottom: 5,
+    paddingLeft: 135,
     paddingRight: 5,
     paddingTop: 5,
     paddingBottom: 5,
     overflow: "visible",
     alignSelf: "center",
-    fontSize: 12,
+    fontSize: 14,
     color: "#ffffff",
     backgroundColor: "#ffffff",
     fontStyle: "normal",
@@ -359,7 +519,7 @@ const styles = StyleSheet.create({
     borderTopWidth: 0,
     borderBottomWidth: 0,
     borderRadius: 0,
-    textAlign: "center",
+    textAlign: "left",
     textAlignVertical: "center",
     textDecorationLine: "none",
     textTransform: "none",
@@ -367,7 +527,39 @@ const styles = StyleSheet.create({
     letterSpacing: 0
   },
   Button_9: {
-    width: "50%",
+    width: "100%",
+    marginLeft: 0,
+    marginRight: 0,
+    marginTop: 5,
+    marginBottom: 5,
+    paddingLeft: 5,
+    paddingRight: 5,
+    paddingTop: 5,
+    paddingBottom: 5,
+    overflow: "hidden",
+    alignSelf: "center",
+    fontSize: 12,
+    color: "#ffffff",
+    backgroundColor: "#ffffff",
+    fontStyle: "normal",
+    fontWeight: "normal",
+    borderColor: "#000000",
+    borderStyle: "solid",
+    borderWidth: 0,
+    borderLeftWidth: 0,
+    borderRightWidth: 0,
+    borderTopWidth: 0,
+    borderBottomWidth: 0,
+    borderRadius: 0,
+    textAlign: "center",
+    textAlignVertical: "center",
+    textDecorationLine: "none",
+    textTransform: "none",
+    lineHeight: 12,
+    letterSpacing: 0
+  },
+  Button_11: {
+    width: "100%",
     marginLeft: 0,
     marginRight: 0,
     marginTop: 5,
@@ -378,7 +570,7 @@ const styles = StyleSheet.create({
     paddingBottom: 5,
     overflow: "visible",
     alignSelf: "center",
-    fontSize: 12,
+    fontSize: 14,
     color: "#ffffff",
     backgroundColor: "#ffffff",
     fontStyle: "normal",
